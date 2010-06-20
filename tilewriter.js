20499f2cd85b7f408b669c0c8a9368dacb141af5
Chester <cd@pobox.com>
2010-06-20 13:27:19 -0300

    fixed IE bug

--- tilewriter.js
+++ tilewriter.js
@@ -3,8 +3,8 @@
 // http://github.com/chesterbr/tilewriter
 //
 // It requires jQuery, any version will do.
-// You should use a minified version. Current version (0.1) can be found at 
-//    http://chester.blog.br/tilewriter/tilewriter-0.1-min.js
+// You should use a minified version. Current version can be found at 
+//    http://chester.blog.br/tilewriter/tilewriter-VERSIONNUMBER-min.js
 // 
 // Copyright (c) 2010 Carlos Duarte do Nascimento (Chester)
 //
